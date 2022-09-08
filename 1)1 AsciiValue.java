483182a9027a37ddca4c3a1775a2cde006c5227f
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-08 08:51:23 +0530

    Create 1)1 AsciiValue.java

---
+++ 1)1 AsciiValue.java
@@ -0,0 +1,10 @@
+Java program to find ASCII value of a character.
+   import java.io.*;   
+     Public class AsciiValue{
+       Public Static void main(String args[])
+       {
+         char ch='d';
+         int ascii=ch;
+         System.out.println("The ASCII value of "+ch+"is:"+ascii);
+       }
+     }
