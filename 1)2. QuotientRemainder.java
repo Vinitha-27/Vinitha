f3a0731c44cb6eb38b90122ed49870ded3c51382
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-08 13:05:31 +0530

    Create 1)2. QuotientRemainder.java

---
+++ 1)2. QuotientRemainder.java
@@ -0,0 +1,11 @@
+import java.io.*;
+Public class Quotient Remainder{
+  Public static void main(String args[])
+  {
+    int dividend=12, divisor=15;
+    int quotient = dividend/divisor;
+    int remainder = dividend % divisor;
+    System.Out.Println("Quotient="+quotient);
+    System.Out.Println("Remainder="+remainder);
+  }
+}
